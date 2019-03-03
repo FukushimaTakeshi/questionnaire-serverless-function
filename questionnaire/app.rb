de4da3a365d741fcbaa1f62977ff3afb0d057352
FukushimaTakeshi <whippet.818@gmail.com>
2019-03-03 20:52:11 +0900

    fix

--- questionnaire/app.rb
+++ questionnaire/app.rb
@@ -12,28 +12,6 @@ def lambda_handler(event:, context:)
     body: CGI.parse(event['body']).to_json
   )
 
-  p '--------------'
-  p context
-
-  # client = Aws::S3::Client.new(
-  #           region: 'ap-northeast-1'
-  #         )
-  #
-  # hoge = {
-  #   hoge: 'fuga',
-  #   hogehoge: [
-  #     1,
-  #     2,
-  #     3,
-  #   ]
-  # }
-  #
-  # client.put_object(
-  #   bucket: 'questionnaire-on-study-group-fukushima',
-  #   key: 'sample.json',
-  #   body: hoge.to_json
-  # )
-  #
   {
     statusCode: 200,
     body: {
